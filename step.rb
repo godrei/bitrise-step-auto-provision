a92b48605ef6f4e994bdae6b35be78923f13590b
Krisztian Godrei <godreikrisztian@gmail.com>
2017-06-15 16:58:29 +0200

    fix

--- step.rb
+++ step.rb
@@ -12,7 +12,7 @@ apple_developer_portal_username = ENV['APPLE_DEVELOPER_PORTAL_USERNAME']
 apple_developer_portal_password = ENV['APPLE_DEVELOPER_PORTAL_PASSWORD']
 apple_developer_portal_team_id = ENV['APPLE_DEVELOPER_PORTAL_TEAMID']
 # generate web session locally by calling: `fastlane spaceauth -u <apple_developer_portal_username>`
-apple_developer_portal_session = ENV['APPLE_DEVELOPER_PORTAL_SESSION']
+# apple_developer_portal_session = ENV['APPLE_DEVELOPER_PORTAL_SESSION']
 apple_developer_portal_app_specific_password = ENV['APPLE_DEVELOPER_PORTAL_APP_SPECIFIC_PASSWORD']
 
 # ---
@@ -21,7 +21,7 @@ apple_developer_portal_app_specific_password = ENV['APPLE_DEVELOPER_PORTAL_APP_S
 
 log_info('Authentication')
 
-ENV['FASTLANE_SESSION'] = apple_developer_portal_session
+# ENV['FASTLANE_SESSION'] = apple_developer_portal_session
 ENV['FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD'] = apple_developer_portal_app_specific_password
 client = Spaceship::Portal.login(apple_developer_portal_username, apple_developer_portal_password)
 client.team_id = apple_developer_portal_team_id
