7096a1e0750ea9dffa38c12e0114e1a9b197f8bc
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-11-12 20:49:20 +0100

    common name fix

--- step.rb
+++ step.rb
@@ -122,7 +122,9 @@ def split_pipe_separated_list(list)
 end
 
 def certificate_common_name(certificate)
-  certificate.subject.to_a.find { |name, _, _| name == 'CN' }[1]
+  common_name = certificate.subject.to_a.find { |name, _, _| name == 'CN' }[1]
+  common_name = common_name.force_encoding('UTF-8')
+  common_name
 end
 
 def certificate_team_id(certificate)
